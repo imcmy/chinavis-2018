48584a654699591943a98ed76cd4a92ddfe941de
CHEN Mingyi <imcmy@outlook.com>
2018-05-15 16:59:01 +0800

    Add structure

--- app/views.py
+++ app/views.py
@@ -1,8 +1,19 @@
-from flask import Blueprint, request
+import os
+from .models import User
+from flask import Blueprint, request, jsonify, make_response
 
 view = Blueprint('view', __name__, url_prefix='/')
+
+base_dir = os.path.dirname(__file__)
 
 
 @view.route('/hello', methods=('GET', 'POST'))
 def hello():
     return "Hello World"
+
+
+@view.route('/users', methods=('GET',))
+def users():
+    resp = make_response(open(os.path.join(base_dir, 'struct.json')).read())
+    resp.headers["Content-type"] = "application/json;charset=UTF-8"
+    return resp
