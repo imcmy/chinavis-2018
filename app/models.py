59465d2fd98d19e6feb47427405ea34accba06bc
CHEN Mingyi <imcmy@outlook.com>
2018-05-15 20:52:16 +0800

    Add email count

--- app/models.py
+++ app/models.py
@@ -14,3 +14,18 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.id
+
+
+class Email(db.Model):
+    __tablename__ = 'email'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.DateTime)
+    proto = db.Column(db.String(255))
+    sip = db.Column(db.String(255))
+    sport = db.Column(db.Integer)
+    dip = db.Column(db.String(255))
+    dport = db.Column(db.Integer)
+    sender = db.Column(db.String)
+    receiver = db.Column(db.String)
+    subject = db.Column(db.String)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 import os
-from .models import User
+from .models import Email
 from flask import Blueprint, request, jsonify, make_response
 
 view = Blueprint('view', __name__, url_prefix='/')
@@ -15,5 +15,29 @@ def hello():
 @view.route('/users', methods=('GET',))
 def users():
     resp = make_response(open(os.path.join(base_dir, 'struct.json')).read())
+    resp.headers["Content-type"] = "application/json;charset=UTF-8"
+    return resp
+
+
+@view.route('/emails', methods=('GET',))
+def emails():
+    # tmp_nodes = list()
+    # tmp_edges = list()
+    # for email in Email.query.all():
+    #     if email.sender.endswith('hightech.com'):
+    #         tmp_nodes.append(email.sender)
+    #         receivers = email.receiver.split(';')
+    #         for receiver in receivers:
+    #             if receiver.endswith('hightech.com'):
+    #                 tmp_edges.append((email.sender, receiver))
+    # nodes = list()
+    # for node in set(tmp_nodes):
+    #     nodes.append({"id": node, "name": node, "weight": tmp_nodes.count(node)})
+    # edges = list()
+    # for index, edge in enumerate(set(tmp_edges)):
+    #     edges.append({"id": index, "source": edge[0], "target": edge[1], "weight": tmp_edges.count(edge)})
+    #
+    # return jsonify({"nodes": nodes, "links": edges})
+    resp = make_response(open(os.path.join(base_dir, 'emails.json')).read())
     resp.headers["Content-type"] = "application/json;charset=UTF-8"
     return resp
