ff66326f2b56d52709fee0fb59fdd7d0d56a2ff2
xuanzebi <906848249@qq.com>
2018-06-02 15:26:09 +0800

    update

--- app/chinavis_data.py
+++ app/chinavis_data.py
@@ -21,7 +21,7 @@ def test():
 def Person_data(post_id):
     if(post_id == 1487 or post_id == 1284):
         da = json.loads(open(os.path.join(base_dir, post_id.__str__()+'.json')).read())
-        return json.dumps(da, ensure_ascii=False)
+        return jsonify(da)
     else:
         data = {
             'ip': '',
